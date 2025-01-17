01c525c27603e5e1e694f9999659619d0046320c
athul or <athulor452@gmail.com>
2025-01-17 09:55:33 +0530

    toast message updated

--- force-app/main/default/lwc/createContact/createContact.js
+++ force-app/main/default/lwc/createContact/createContact.js
@@ -1,35 +1,67 @@
-import { LightningElement, track } from 'lwc';
-import createContact from '@salesforce/apex/ContactController.createContact';
+import { LightningElement, track } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent"; 
+import createContact from "@salesforce/apex/ContactController.createContact";
 
 export default class CreateContact extends LightningElement {
-    @track contact = {
-        FirstName: '',
-        LastName: '',
-        Email: '',
-        Phone: '',
-        MailingStreet: ''
+  @track contact = {
+    FirstName: "",
+    LastName: "",
+    Email: "",
+    Phone: "",
+    MailingStreet: "",
+    MailingCity: "",
+    MailingState: "",
+    MailingPostalCode: "",
+    MailingCountry: "",
+  };
+
+  handleInputChange(event) {
+    const field = event.target.name;
+    this.contact[field] = event.target.value;
+  }
+
+  saveContact() {
+    createContact({ contactRecord: this.contact })
+      .then((result) => {
+        this.showToast("Success", "Contact created successfully!", "success");
+        this.clearForm();
+      })
+      .catch((error) => {
+
+        this.showToast(
+          "Error",
+          "Unable to save contact. " + error.body.message,
+          "error"
+        );
+      });
+  }
+
+
+  showToast(title, message, variant) {
+    const event = new ShowToastEvent({
+      title: title,
+      message: message,
+      variant: variant,
+    });
+    this.dispatchEvent(event);
+  }
+
+
+  clearForm() {
+    this.contact = {
+      FirstName: "",
+      LastName: "",
+      Email: "",
+      Phone: "",
+      MailingStreet: "",
+      MailingCity: "",
+      MailingState: "",
+      MailingPostalCode: "",
+      MailingCountry: "",
     };
-    @track message = '';
-    @track error = false;
 
-    handleInputChange(event) {
-        const field = event.target.name;
-        this.contact[field] = event.target.value;
-    }
-
-    saveContact() {
-        createContact({ contactRecord: this.contact })
-            .then(result => {
-                this.message = result;
-                this.error = false;
-            })
-            .catch(error => {
-                this.message = 'Error: Unable to save contact. ' + error.body.message;
-                this.error = true;
-            });
-    }
-
-    get hasMessage() {
-        return this.message !== '';
-    }
+    this.template.querySelectorAll("lightning-input, lightning-textarea").forEach((input) => {
+      input.value = "";
+    });
+  }
 }
